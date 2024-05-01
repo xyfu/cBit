679e6abfc54925f096ee739d78e18e0d0fd38bc7
xyfu <jack@xnux.net>
2024-05-01 09:01:09 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ load dependency
 
 
 //% color="#C814B8" weight=25 icon="\uf1d4"
-namespace cbit_显示类 {
+namespace cbit_display {
     
     export enum enColor {
 
@@ -161,7 +161,7 @@ namespace cbit_显示类 {
  ****************************************************************************************************************************************/
 
 //% color="#87CEEB" weight=24 icon="\uf1b6"
-namespace cbit_传感器类 {
+namespace cbit_sensor {
 
     export enum enVoice {
         //% blockId="Voice" block="有声音"
@@ -259,7 +259,7 @@ namespace cbit_传感器类 {
  ****************************************************************************************************************************************/
 
 //% color="#808080" weight=23 icon="\uf11c"
-namespace cbit_输入类 {
+namespace cbit_input {
 
     export enum enRocker {
         //% blockId="Nostate" block="无"
@@ -370,7 +370,7 @@ namespace cbit_输入类 {
  ****************************************************************************************************************************************/
 
 //% color="#D2691E" weight=22 icon="\uf001"
-namespace cbit_音乐类 {
+namespace cbit_music {
     export enum enBuzzer {
 
         //% blockId="NoBeep" block="响"
@@ -399,7 +399,7 @@ namespace cbit_音乐类 {
  ****************************************************************************************************************************************/
 
 //% color="#0000CD" weight=21 icon="\uf185"
-namespace cbit_电机类 {
+namespace cbit_motor {
 
     //% blockId=cbit_Fan block="风扇|引脚 %pin|速度 %value"
     //% weight=100
@@ -428,7 +428,7 @@ namespace cbit_电机类 {
 }
 
 //% color="#006400" weight=20 icon="\uf1b9"
-namespace cbit_小车类 {
+namespace cbit_bot {
 
     const PCA9685_ADD = 0x41
     const MODE1 = 0x00
